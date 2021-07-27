7c969019a06c1dbd1e78bf0a8d9c9fc92c20c57a
YaelPM <193259@ids.upchiapas.edu.mx>
2021-07-27 17:24:55 -0500

    Cambio de turnos y metodo limpiar

--- models/socket.js
+++ models/socket.js
@@ -1,21 +1,42 @@
 class Sockets {
 
+
     constructor(io) {
         this.io = io;
 
         this.socketEvents();
         this.players=[]
+        this.player=0;
+    }
+    twoPlayers(){
+
     }
 
     socketEvents() {
         this.io.on('connection', client => {
-
+            
             client.on('msj-input-server', (data) => {
                 console.log(data);
 
                 this.io.emit('msj-output-client', data);
             })
-            
+
+            client.on('limpiar', () => {
+                this.io.emit('clear');
+            })
+
+            client.on('listo', ()=>{
+
+                this.player++
+                this.players.push(this.player)
+                console.log(this.player)
+                if(this.player%2==1){
+                    client.emit('cambio', 'O')
+                }
+            })
+
+
+            console.log(this.players)
             
             client.emit('connection', 'Conexion exitosa')
 
